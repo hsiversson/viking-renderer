60b272a32c7d3d982f82ace9d22b903dbc90a799
eduwushu <edu.kungfu@gmail.com>
2025-07-18 16:54:23 +0200

    Nocco Miami sky
    Nocco Miami Sky

--- content/shaders/sky.hlsl
+++ content/shaders/sky.hlsl
@@ -9,6 +9,17 @@ cbuffer PerBatchConstantBuffer : register(b0)
 [numthreads(8,8,1)]
 void MainCS(uint3 dispatchThreadID : SV_DispatchThreadID)
 {
+    static const int ColorCount = 6;
+    float4 sunsetColors[ColorCount] =
+    {
+        float4(0.05, 0.10, 0.30, 1.0), // Deep blue (dusk sky)
+    float4(0.35, 0.10, 0.40, 1.0), // Violet
+    float4(0.70, 0.20, 0.50, 1.0), // Magenta/pink
+    float4(0.95, 0.45, 0.30, 1.0), // Orange
+    float4(1.00, 0.70, 0.30, 1.0), // Golden orange
+    float4(1.00, 0.85, 0.60, 1.0) // Pale yellow/gold (sun glow)
+    };
+    
     RWTexture2D<float4> BackbufferTexture = ResourceDescriptorHeap[ BackbufferDescriptorIndex];
     Texture2D<float> DepthTexture = ResourceDescriptorHeap[DepthbufferDescriptorIndex];
     
@@ -20,6 +31,16 @@ void MainCS(uint3 dispatchThreadID : SV_DispatchThreadID)
     
     float depth = DepthTexture.Load(int3(dispatchThreadID.xy, 0)).r;
     if (depth == 0.0f)
-        BackbufferTexture[dispatchThreadID.xy] = float4(0, 0, 1, 1);
+    {
+        float t = dispatchThreadID.y / (float) (height - 1);
+        t = saturate(t);
+        float scaled = t * (ColorCount - 1);
+        int i = (int) floor(scaled);
+        float localT = frac(scaled);
+        i = clamp(i, 0, ColorCount - 2);
+        float4 SunsetColor = lerp(sunsetColors[i], sunsetColors[i + 1], localT);
+        
+        BackbufferTexture[dispatchThreadID.xy] = SunsetColor;
+    }
 
 }
