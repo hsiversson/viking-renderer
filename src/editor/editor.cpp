b75ae1f506db47f41b79995e35337f4472d16c40
Hampus Siversson <hampus.siversson@gmail.com>
2025-07-21 10:54:52 +0200

    Added Editor stub

---
+++ src/editor/editor.cpp
@@ -0,0 +1,26 @@
+#include "editor/editor.h"
+
+#if ENABLE_EDITOR
+namespace vkr::Editor
+{
+	Manager::Manager()
+	{
+
+	}
+
+	Manager::~Manager()
+	{
+
+	}
+
+	void Manager::Update()
+	{
+
+	}
+
+	void Manager::Draw()
+	{
+
+	}
+}
+#endif // ENABLE_EDITOR
---
+++ src/editor/editor.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#if ENABLE_EDITOR
+namespace vkr::Editor
+{
+	class Manager
+	{
+	public:
+		Manager();
+		~Manager();
+
+		void Update();
+
+		void Draw();
+
+	private:
+	};
+}
+#endif // ENABLE_EDITOR
