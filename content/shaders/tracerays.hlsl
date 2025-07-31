4e38c35d3903470336808333fc36668b4db4d610
eduwushu <edu.kungfu@gmail.com>
2025-07-31 09:45:02 +0200

    RT Sky
    Added sky rendering to RT shader

--- content/shaders/tracerays.hlsl
+++ content/shaders/tracerays.hlsl
@@ -265,6 +265,52 @@ void Main(uint3 DispatchThreadId : SV_DispatchThreadID)
         //    // add diffuse indirect contrib and make sure to weight properly against brdf
         //}
     }
-    
+    else
+    {
+        //Sky rendering
+        static const int ColorCount = 6;
+        float4 sunsetColors[ColorCount] =
+        {
+            float4(0.05, 0.10, 0.30, 1.0), // Deep blue (dusk sky)
+            float4(0.35, 0.10, 0.40, 1.0), // Violet
+            float4(0.70, 0.20, 0.50, 1.0), // Magenta/pink
+            float4(0.95, 0.45, 0.30, 1.0), // Orange
+            float4(1.00, 0.70, 0.30, 1.0), // Golden orange
+            float4(1.00, 0.85, 0.60, 1.0) // Pale yellow/gold (sun glow)
+        };
+        
+        float3 camToPixel = ro.xyz - SceneConstants.CameraPosition;
+        
+        float3 toPixelDir = normalize(camToPixel);
+        
+        float t = 1.0 - abs(dot(toPixelDir, float3(0, 1, 0)));
+        
+        t = saturate(t);
+        float scaled = t * (ColorCount - 1);
+        int i = (int) floor(scaled);
+        float localT = frac(scaled);
+        i = clamp(i, 0, ColorCount - 2);
+        float4 SunsetColor = lerp(sunsetColors[i], sunsetColors[i + 1], localT);
+        
+        // Calculate simple sun disk
+        for (uint lightIdx = 0; lightIdx < SceneConstants.NumDirectionalLightsInUse; ++lightIdx)
+        {
+            const DirectionalLightData dirLight = SceneConstants.DirectionalLights[lightIdx];
+
+            float sunDot = dot(toPixelDir, -normalize(dirLight.Direction)); // cos(angle)
+            float cosInner = cos(dirLight.Radius); // Hard edge
+            float cosOuter = cos(dirLight.Radius * 2.0f); // Feathered falloff
+
+            float sunFactor = saturate((sunDot - cosOuter) / (cosInner - cosOuter));
+
+            // Optionally apply power falloff for a softer edge
+            sunFactor = pow(sunFactor, 4.0); // tweak this for sharpness
+            
+            float3 sunDiskColor = dirLight.Emission * sunFactor;
+            SunsetColor.rgb += sunDiskColor;
+        }
+        
+        resultColor = SunsetColor;
+    }
     SceneTexture[pixel] = resultColor;
 }
