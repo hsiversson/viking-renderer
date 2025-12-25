7c4fd387da7d6fcea3274f62f4efde3ec27b5426
Hampus Siversson <hampus.siversson@gmail.com>
2025-12-25 19:57:00 +0100

    dlss settings init fix

--- src/application/appsettings.cpp
+++ src/application/appsettings.cpp
@@ -9,6 +9,10 @@ namespace vkr
 	{
 		VKR_ASSERT(g_Instance == nullptr);
 		g_Instance = this;
+
+		// Only inited here because of the union, we should have a default function setting all the defaults and then save/load from disk settings.
+		m_GraphicsSettings.m_DLSS.m_QualityMode = DLSS_QUALITY_MODE_BALANCED;
+		m_GraphicsSettings.m_DLSS.m_UseRayReconstruction = true;
 	}
 
 	vkr::GraphicsSettings& AppSettings::GetGraphicsSettings()
